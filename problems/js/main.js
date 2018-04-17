5782ef70563484f06d006ae15cb015eb8f0beb44
Anurag <anuragvns1111@gmail.com>
2018-04-17 19:12:05 +0530

    v1.0

--- problems/js/main.js
+++ problems/js/main.js
@@ -118,4 +118,21 @@ function renderHTML(data) {
   animalContainer.insertAdjacentHTML('beforeend', htmlString);
 }
 
+function myFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("set");
+  li = ul.getElementsByTagName("tr");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("td")[2];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
+
 });
