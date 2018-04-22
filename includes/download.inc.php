1cc37b94d50ee752f8bf66b58b52fe5aacdcdcbc
Anurag <anuragvns1111@gmail.com>
2018-04-22 23:55:47 +0530

    cs

--- includes/download.inc.php
+++ includes/download.inc.php
@@ -8,32 +8,32 @@
   	$user=strtoupper($user);
 
 
-if (isset($_GET['file']) && basename($_GET['file']) == $_GET['file']) {
-	$filename = $_GET['file'].'.c';
-} else {
-	$filename = NULL;
-}
+	if (isset($_GET['file']) && basename($_GET['file']) == $_GET['file']) {
+		$filename = $_GET['file'].'.c';
+	} else {
+		$filename = NULL;
+	}
 
-if ($filename) {
-	// define the path to your download folder plus assign the file name
-	$path = '/opt/lampp/htdocs/project/visualizer/uploads/'.$user.'/'.$filename;
-	// check that file exists and is readable
-	if (file_exists($path) && is_readable($path)) {
-		// get the file size and send the http headers
-		$size = filesize($path);
-		header('Content-Type: application/octet-stream');
-		header('Content-Length: '.$size);
-		header('Content-Disposition: attachment; filename='.$filename);
-		header('Content-Transfer-Encoding: binary');
-		// open the file in binary read-only mode
-		// display the error message if file can't be opened
-		$file = @ fopen($path, 'rb');
-		if ($file) {
+	if ($filename) {
+		// define the path to your download folder plus assign the file name
+		$path = '/opt/lampp/htdocs/project/visualizer/uploads/'.$user.'/'.$filename;
+		// check that file exists and is readable
+		if (file_exists($path) && is_readable($path)) {
+			// get the file size and send the http headers
+			$size = filesize($path);
+			header('Content-Type: application/octet-stream');
+			header('Content-Length: '.$size);
+			header('Content-Disposition: attachment; filename='.$filename);
+			header('Content-Transfer-Encoding: binary');
+			// open the file in binary read-only mode
+			// display the error message if file can't be opened
+			$file = @ fopen($path, 'rb');
+			if ($file) {
 			// stream the file and exit the script when complete
 			fpassthru($file);
 			exit;
 		}
 	}
-} else {
-	echo 'Server Error';
-}
+	} else {
+		echo 'Server Error';
+	}
